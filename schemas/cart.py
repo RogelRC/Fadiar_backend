d484411390d453e82b981f1f324455489dc71a3b
RogelRC <rogelprogrammer@gmail.com>
2025-02-23 01:38:03 -0500

    Fixes

--- schemas/cart.py
+++ schemas/cart.py
@@ -1,5 +1,6 @@
 # schemas/cart.py
 from pydantic import BaseModel
+from datetime import datetime
 
 class CartBase(BaseModel):
     product_id: int
@@ -15,6 +16,7 @@ class Cart(CartBase):
     id: int
     user_id: int
     is_purchased: bool
+    added_at: datetime  # Add this line
 
     class Config:
         orm_mode = True
